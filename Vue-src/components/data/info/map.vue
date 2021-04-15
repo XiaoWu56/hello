d11bb2ba28b5cf0516eb02f658855d9d28a50344
XiaoWu56 <82553369+XiaoWu56@users.noreply.github.com>
2021-04-15 17:47:18 +0800

    Add files via upload

---
+++ Vue-src/components/data/info/map.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="map">
+        <div id="chinaMap" style="width: 71vw;height: 54.5vh;"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'map',
+    props: {
+        num: 0
+    },
+    methods: {
+        drawLine(){
+            var myChart = echarts.init(document.getElementById("chinaMap"));
+            /**
+             *  指定图表的配置项和数据
+             * */
+            var option= {
+                backgroundColor: '#fff',  //设置背景颜色
+                title: {
+                    show:true,
+                    text: '中国地图',
+                    subtext: '',
+                    x:'center'
+                },
+                tooltip : {
+                    trigger: 'item'
+                },
+                //左侧小导航图标
+                visualMap: {
+                    show : true,
+                    x: 'left',
+                    y: 'bottom',
+                    splitList: [
+                        {start: 10, end:20},
+                        {start: 6, end: 10},
+                        {start: 0, end: 6},
+                    ],
+                    color: ['#a5ad00', '#8bdd00', '#2cd493']
+                },
+                //配置属性
+                series: [{
+                    name: '数量',
+                    type: 'map',
+                    mapType: 'china',
+                    roam: false,
+                    zoom:1.2,
+                    label: {
+                        normal: {
+                            show: false ,
+                        },
+                        emphasis: {
+                            show: false
+                        }
+                    },
+                    data:[{name: '广东',value:this.num }]
+                }]
+            };
+            myChart.setOption(option);
+                }
+            },
+            mounted() {
+                this.drawLine();
+                
+        },
+}
+</script>
+
+<style scoped>
+.map{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+}
+</style>
---
+++ Vue-src/components/data/info/nums.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="nums">
+        <ul class="numsUl">
+            <li><h4 class="blueColor">{{numList[0]}}</h4><br/>问卷总数<span class="el-icon-question"></span></li>
+            <li><h4 class="blueColor">{{numList[1]}}</h4><br/>今日新增<span class="el-icon-question"></span></li>
+            <li><h4>{{numList[2]/1000}}</h4><br/>平均填写时长（秒）<span class="el-icon-question"></span></li>
+            <li><h4>{{numList[3]}}</h4><br/>问卷被浏览<span class="el-icon-question"></span></li>
+            <li class="nomargin"><h4>{{numList[4]}}</h4><br/>结果被浏览<span class="el-icon-question"></span></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'nums',
+    props:{
+        numList:{
+            type: Array,
+            default(){
+                return [111,0,0,0,0]
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.nums{
+    width: 100%;
+    height: 100%;
+}
+.nums .numsUl{
+    width: 100%;
+    height: 100%;
+}
+.nums .numsUl li h4{
+    display: inline;
+    text-align: center;
+    line-height: 10vh;
+    font-size: 2vw;
+    font-weight: normal;
+}
+.nums .numsUl li{
+    width: calc(100% / 5 - 1.7% - 1px);
+    height: 100%;
+    border: 1px solid gainsboro;
+    float: left;
+    background: #fff;
+    margin-right:  2%;
+    text-align: center;
+}
+.nums .numsUl .nomargin{
+    margin-right: 0;
+}
+.blueColor{
+    color:rgb(0, 119, 255);
+}
+</style>
